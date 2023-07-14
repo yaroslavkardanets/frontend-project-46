9b1a3acca852dd928a3cd2fac3d8b7f265dc4efd
kardanets@gmail.com <kardanets@gmail.com>
2023-07-14 22:08:37 +0300

    step 2. Добавление option -f

--- gendiff.js
+++ gendiff.js
@@ -5,6 +5,7 @@ const { program } = require('commander');
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.0');
+  .version('0.1.0')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
