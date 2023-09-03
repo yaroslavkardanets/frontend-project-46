89144e9758f902d7d405f49856e2c7c3e3f0f08c
kardanets@gmail.com <kardanets@gmail.com>
2023-09-03 08:13:48 +0300

    step 2. Добавление argument

--- gendiff.js
+++ gendiff.js
@@ -6,6 +6,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
-  .option('-f, --format <type>', 'output format');
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1> <filepath2>');
 
 program.parse();
+
