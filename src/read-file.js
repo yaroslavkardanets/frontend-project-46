cc506f75b627aaa1a44a97be992659d8a2a3ae21
kardanets@gmail.com <kardanets@gmail.com>
2023-11-26 21:44:33 +0300

    step 3. Упростил функцию чтения пути

---
+++ src/read-file.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+
+const readFilePath = (filePath) => {
+	// const absolutePath = path.isAbsolute(filePath) ? filePath : path.resolve(process.cwd(), filePath);
+	const absolutePath = path.resolve(process.cwd(), filePath);
+	console.log('absolutePath =', absolutePath);
+	const getFile = JSON.parse(fs.readFileSync(absolutePath));
+	console.log('getFile =', getFile);
+	return getFile;
+};
+
+console.log(path.resolve(process.cwd()));
+console.log(path.resolve('../__fixtures__/file2.json'));
+
+console.log(readFilePath('/mnt/c/Slow/Hexlet/frontend-project-46/__fixtures__/file2.json'))
+console.log(readFilePath('../__fixtures__/file2.json'))
+
+// export default readFilePath;
